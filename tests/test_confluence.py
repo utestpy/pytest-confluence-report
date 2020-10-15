d47778cda65ebd7ed10ceefb3b53fe467afe7354
Volodymyr <vyahello@gmail.com>
2020-10-15 21:07:25 +0300

    Add empty page unit tests

--- tests/test_confluence.py
+++ tests/test_confluence.py
@@ -1,7 +1,21 @@
+import pytest
 from atlassian import Confluence
-from report.confluence import _client_from_settings
+from report.confluence import Page, _EmptyPage, _client_from_settings
 from report.settings import Settings
+
+
+@pytest.fixture(scope='module')
+def empty_page() -> Page:
+    return _EmptyPage()
 
 
 def test_client_from_settings(fake_settings: Settings) -> None:
     assert isinstance(_client_from_settings(fake_settings), Confluence)
+
+
+def test_empty_link(empty_page: Page) -> None:
+    assert not empty_page.link
+
+
+def test_empty_build(empty_page: Page) -> None:
+    assert not empty_page.build('')

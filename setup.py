f2333aecd8bb8e610a50f642a4d55cc6ffa33a22
Volodymyr <vyahello@gmail.com>
2020-10-26 21:18:25 +0200

    Release package setup from internal python dependencies

--- setup.py
+++ setup.py
@@ -1,18 +1,68 @@
 """Package setup entrypoint."""
+import re
 import sys
+from pathlib import Path
 from typing import IO, Sequence
 from setuptools import (
     find_packages as __find_packages,
     setup as __compose_package,
 )
-from report import (
-    __author__ as __author,
-    __doc__ as __full_doc,
-    __email__ as __email,
-    __license__ as __license,
-    __package_name as __name,
-    __version__ as __version,
-)
+
+
+class __Package:
+    """Represents a single package."""
+
+    def __init__(self, path: Path) -> None:
+        self._path = path
+
+    @property
+    def version(self) -> str:
+        """Returns a package version e.g `0.0.1`."""
+        return self._read(pattern=r"(?<=__version__: str = ')[\d\.\d\.\d]+")
+
+    @property
+    def name(self) -> str:
+        """Returns a package name e.g `some-package`."""
+        return self._read(
+            pattern=r"(?<=__package_name__: str = ')[\w-]+",
+        )
+
+    @property
+    def author(self) -> str:
+        """Returns a package author e.g `some-author`."""
+        return self._read(
+            pattern=r"(?<=__author__: str = ')[\w-]+",
+        )
+
+    @property
+    def license_(self) -> str:
+        """Returns a package license e.g `MIT`."""
+        return self._read(
+            pattern=r"(?<=__license__: str = ')[\w-]+",
+        )
+
+    @property
+    def email(self) -> str:
+        """Returns a package email e.g `some-email@com`."""
+        return self._read(
+            pattern=r"(?<=__email__: str = ')[\w-]+",
+        )
+
+    @property
+    def goal(self) -> str:
+        """Returns a package goal e.g `reporting results`."""
+        return self._read(
+            pattern=r'(?<=""")[\w\s\.]+',
+        )
+
+    def _read(self, pattern: str) -> str:
+        """Reads package content.
+
+        :param pattern: <str> a package regular expression pattern.
+        """
+        return re.findall(
+            pattern=pattern, string=(self._path / '__init__.py').open().read()
+        )[0]
 
 
 def __readme() -> str:
@@ -27,27 +77,17 @@ def __requirements() -> Sequence[str]:
         return tuple(map(str.strip, requirements.readlines()))
 
 
-def __first_line(string: str, delimiter: str = "\n") -> str:
-    """Returns only first line up to next delimiter item occurred.
-
-    Args:
-        string (str): given string item
-        delimiter (str): separator string
-    """
-    return string.split(delimiter)[0]
-
-
-def main() -> None:
+def main(package: __Package) -> None:
     """Setup package entrypoint."""
     __compose_package(
-        name=__name,
-        version=__version,
-        author=__author,
-        author_email=__email,
-        description=__first_line(__full_doc),
+        name=package.name,
+        version=package.version,
+        author=package.author,
+        author_email=package.email,
+        description=package.goal,
         long_description=__readme(),
         long_description_content_type="text/markdown",
-        url=f"https://github.com/vyahello/{__name}",
+        url=f"https://github.com/vyahello/{package.name}",
         packages=__find_packages(
             exclude=("*.tests", "*.tests.*", "tests.*", "tests")
         ),
@@ -59,7 +99,7 @@ def main() -> None:
             "Programming Language :: Python :: 3.6",
             "Programming Language :: Python :: 3.7",
             "Programming Language :: Python :: 3.8",
-            f"License :: OSI Approved :: {__license} License",
+            f"License :: OSI Approved :: {package.license_} License",
             "Operating System :: OS Independent",
         ),
         python_requires=">=3.6",
@@ -68,4 +108,4 @@ def main() -> None:
 
 
 if __name__ == "__main__":
-    sys.exit(main())
+    sys.exit(main(package=__Package(path=Path('report'))))

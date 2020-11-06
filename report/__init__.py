ce5fcd307317ad0da1b0cdeb9ae28dfed2cada82
Volodymyr <vyahello@gmail.com>
2020-11-06 09:25:39 +0200

    Release 0.0.3 version

--- report/__init__.py
+++ report/__init__.py
@@ -19,7 +19,7 @@ __author__: str = 'Volodymyr Yahello'
 __email__: str = 'vyahello@gmail.com'
 __license__: str = 'MIT'
 __copyright__: str = f'Copyright 2020, {__author__}'
-__version__: str = '0.0.2'
+__version__: str = '0.0.3'
 __package_name__: str = 'pytest-confluence-report'
 __all__: Tuple[str, ...] = (
     'ConfluenceContent',
